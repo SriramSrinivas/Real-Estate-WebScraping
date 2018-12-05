16ca177ab42f9ec8acb7143e73917ef96047818c
Sriram Srinivasan <sriram882017@gmail.com>
2018-12-04 20:33:55 -0600

    Real

--- propertyappraisal/property/urls.py
+++ propertyappraisal/property/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,url,include
+from appraisal import controllers
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^customers/$',controllers.test),
 ]
